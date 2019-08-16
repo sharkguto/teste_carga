b4db870828f8416d039d84188d0d9d70000b8af5
gustavo <gustavo.freitas@brasil317.com.br>
2019-08-16 18:08:42 -0300

    tunning nim test wrk

--- nim_bench/example.nim
+++ nim_bench/example.nim
@@ -30,3 +30,15 @@ runForever()
 #sudo sysctl -w fs.inotify.max_user_watches=100000
 
 #ulimit -n 63536
+
+
+#sudo sysctl net.core.somaxconn=9086
+#sudo sysctl net.core.netdev_max_backlog=4096
+#sudo sysctl net.ipv4.tcp_max_syn_backlog=4096
+#sudo ifconfig enp3s0 txqueuelen 10000
+#sudo sysctl -p
+
+#sudo sysctl net.ipv4.tcp_fin_timeout=15
+#sudo sysctl net.ipv4.ip_local_port_range="15000 61000"
+
+#wrk -c 2046 -t 4 http://192.168.0.134:8080/json -d 400
