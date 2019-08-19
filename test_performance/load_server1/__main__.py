d13042d197937b55c8fafda1acac800fc90e905b
gustavo <gustavo.freitas@brasil317.com.br>
2019-08-19 17:31:36 -0300

    test connexion

--- test_performance/load_server1/__main__.py
+++ test_performance/load_server1/__main__.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 
 #gunicorn load_server3:app --workers=4 -b "0.0.0.0:8080" --worker-class=aiohttp.GunicornUVLoopWebWorker --chdir load_server3/
 #gunicorn load_server3:app --workers=4 -b "0.0.0.0:8080" --worker-class=aiohttp.worker.GunicornWebWorker --chdir load_server3/
-#gunicorn load_server3:app --workers=4 -b "0.0.0.0:8080" --worker-class=meinheld.gmeinheld.MeinheldWorker --chdir load_server3/
+#gunicorn load_server33:app --workers=4 -b "0.0.0.0:8080" --worker-class=meinheld.gmeinheld.MeinheldWorker --chdir load_server33/
