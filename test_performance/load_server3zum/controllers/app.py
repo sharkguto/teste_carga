719284a11e044dc81c25fa79f894405cbb49c9b7
gustavo <gustavo.freitas@brasil317.com.br>
2019-08-19 16:51:59 -0300

    adjust

--- test_performance/load_server3zum/controllers/app.py
+++ test_performance/load_server3zum/controllers/app.py
@@ -1,9 +1,6 @@
 from connexion.decorators.security import validate_scope
 from connexion.exceptions import OAuthScopeProblem
 
-# from aiohttp import web
-from connexion.lifecycle import ConnexionResponse
-
 
 def basic_auth(username, password, required_scopes=None):
     if username == "admin" and password == "secret":
@@ -31,17 +28,5 @@ def dummy_func(token):
     return None
 
 
-# def get_secret(user) -> str:
-#     return "You are {user} and the secret is 'wbevuec'".format(user=user)
-
-
-async def get_secret(user) -> str:
-    # return "You are {user} and the secret is 'wbevuec'".format(user=user)
-    return ConnexionResponse(
-        body="You are {user} and the secret is 'wbevuec'".format(user=user)
-    )
-
-
-# gunicorn load_server3:application --workers=4 -b "0.0.0.0:8080" --worker-class=aiohttp.GunicornUVLoopWebWorker --chdir load_server3/
-# gunicorn load_server3:application --workers=4 -b "0.0.0.0:8080" --worker-class=aiohttp.worker.GunicornWebWorker --chdir load_server3/
-
+def get_secret(user) -> str:
+    return "You are {user} and the secret is 'wbevuec'".format(user=user)
