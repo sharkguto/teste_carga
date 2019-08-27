60fbfcbc0fa54a2f43bf40a8340177b700e5c39f
gustavo <freitasmgustavo@gmail.com>
2019-08-27 18:13:32 -0300

    nim update bench

--- nim_bench/example.nim
+++ nim_bench/example.nim
@@ -3,12 +3,14 @@ import json
 import pg
 #import db_postgres # -> use libpq
 import lists
+# import threadpool
+#nim c -d:release --threads:off --opt:speed --stackTrace:off example.nim
 
 
-#let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
-let db = newAsyncPool("localhost", "gustavo", "test", "postgres", 10)
+let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
+    #let db = newAsyncPool("localhost", "gustavo", "test", "postgres", 10)
 
-#let db = open("localhost", "gustavo", "test", "postgres")
+    #let db = open("localhost", "gustavo", "test", "postgres")
 
 type
     Row = object
@@ -19,12 +21,30 @@ type
         name: string
 
 
+
+proc rowizer_async(rec: seq[string]): Future[JsonNode] {.async.} =
+    result = %*Row(salary: parseFloat(rec[0]), address: rec[1],
+            age: parseInt(rec[2]), id: parseInt(rec[3]), name: rec[4])
+
 proc rowizer(rec: seq[string]): JsonNode =
     result = %*Row(salary: parseFloat(rec[0]), address: rec[1],
             age: parseInt(rec[2]), id: parseInt(rec[3]), name: rec[4])
 
-proc exec_query(): seq[JsonNode] =
+proc exec_query_async(): Future[seq[JsonNode]] {.async.} =
+    var
+        j: seq[JsonNode]
+        #j {.threadvar.}: seq[JsonNode]
 
+    let rows = await pool.rows(sql"SELECT salary,address,age,id,name FROM test.company",
+            @[])
+    for rec in rows:
+        let row_data = await rowizer_async(rec)
+        j.add(row_data)
+
+    result = j
+
+proc exec_query(): seq[JsonNode] =
+    let db = open("localhost", "gustavo", "test", "postgres")
     var
         j: seq[JsonNode]
 
@@ -32,9 +52,13 @@ proc exec_query(): seq[JsonNode] =
         let row_data = rowizer(rec)
         j.add(row_data)
 
+    db.close()
     result = j
 
 router myrouter:
+    get "/db2":
+        var j_data = await exec_query_async()
+        resp $(%*j_data), "application/json"
     get "/json":
         var j_data = exec_query()
         resp $(%*j_data), "application/json"
@@ -51,4 +75,8 @@ proc main() =
 when isMainModule:
     main()
 
+#ulimit -n 63536
+#nim c -d:release --threads:off --opt:speed --stackTrace:off example.nim -> for async
 #nim c -d:release --threads:on --opt:speed --stackTrace:off example.nim
+#nim c -d:release --threads:on --opt:speed --stackTrace:off --showAllMismatches:on example.nim
+#nimble install jester@#head
--- nim_bench/example_tmp.nim
+++ nim_bench/example_tmp.nim
@@ -1,76 +1,118 @@
 import asyncdispatch, jester, os, strutils
 import json
-#import pg
-import db_postgres # -> use libpq
+import pg
+#import db_postgres # -> use libpq
+import lists
+import threadpool
 
 
-#let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
-let db = open("localhost", "gustavo", "test", "postgres")
+let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
+    #let db = newAsyncPool("localhost", "gustavo", "test", "postgres", 10)
+
+    #let db = open("localhost", "gustavo", "test", "postgres")
+
+type
+    Row = object
+        salary: float
+        address: string
+        age: int
+        id: int
+        name: string
+
+
+
+proc rowizer_async(rec: seq[string]): Future[JsonNode] {.async.} =
+    result = %*Row(salary: parseFloat(rec[0]), address: rec[1],
+            age: parseInt(rec[2]), id: parseInt(rec[3]), name: rec[4])
+
+proc rowizer(rec: seq[string]): JsonNode =
+    result = %*Row(salary: parseFloat(rec[0]), address: rec[1],
+            age: parseInt(rec[2]), id: parseInt(rec[3]), name: rec[4])
+
+proc doWork(evt: AsyncEvent) {.gcsafe.} =
+    sleep(3000)
+    evt.trigger()
+    echo "work over"
+
+proc waitEvent(ev: AsyncEvent): Future[void] =
+    var fut = newFuture[void]("waitEvent")
+    proc cb(fd: AsyncFD): bool = fut.complete(); return true
+    addEvent(ev, cb)
+    return fut
+
+
+
+
+
+proc exec_query_async(): Future[seq[JsonNode]] {.async.} =
+    var
+        j: seq[JsonNode]
+        #j {.threadvar.}: seq[JsonNode]
+
+    let rows = await pool.rows(sql"SELECT salary,address,age,id,name FROM test.company",
+            @[])
+    for rec in rows:
+        let row_data = await rowizer_async(rec)
+        j.add(row_data)
+
+    result = j
+
+proc execQueryAsync(ev: AsyncEvent): Future[seq[JsonNode]] {.async.} =
+    var
+        j: seq[JsonNode]
+        #j {.threadvar.}: seq[JsonNode]
+
+    let rows = await pool.rows(sql"SELECT salary,address,age,id,name FROM test.company",
+            @[])
+    for rec in rows:
+        let row_data = await rowizer_async(rec)
+        j.add(row_data)
+
+    result = j
+
+
+
+proc callAsync(): Future[seq[JsonNode]] {.async.} =
+    let evt = newAsyncEvent()
+    let fut = execQueryAsync(evt)
+    spawn doWork(evt)
+    await fut
+    result = @[]
+
+proc exec_query(): seq[JsonNode] =
+    let db = open("localhost", "gustavo", "test", "postgres")
+    var
+        j: seq[JsonNode]
+
+    for rec in db.fastRows(sql"SELECT salary,address,age,id,name FROM test.company"):
+        let row_data = rowizer(rec)
+        j.add(row_data)
+
+    db.close()
+    result = j
 
 router myrouter:
-  get "/json":
-    let rows = db.fastRows(sql"SELECT 1")
-    for row in rows:
-      echo row
-    resp $(%*{"message": "Hello, World!"}), "application/json"
+    get "/db2":
+        var j_data = await callAsync()
+        resp $(%*j_data), "application/json"
+    get "/json":
+        var j_data = exec_query()
+        resp $(%*j_data), "application/json"
+        #resp $(%*{"message": "Hello, World!"}), "application/json"
 
 proc main() =
-  
-  let port = 8080.Port #paramStr(1).parseInt().Port
-  let settings = newSettings(port = port)
-  var jester = initJester(myrouter, settings = settings)
-  jester.serve()
+
+    let port = 8080.Port #paramStr(1).parseInt().Port
+    let settings = newSettings(port = port)
+    var jester = initJester(myrouter, settings = settings)
+
+    jester.serve()
 
 when isMainModule:
-  main()
-
-# import json
-
-# import jester, asyncdispatch
-# import pg
-# #import db_postgres # -> use libpq
-
-# #let db = open("localhost", "gustavo", "test", "postgres")
-# let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
-
-# settings:
-#   port = Port(8080)
-
-
-# routes:
-#   get "/json":
-#     let rows = waitFor pg.rows(sql"SELECT salary,address,age,id,name FROM test.company")
-
-#     var data = $(%*{"message": "Hello, World!"})
-#     resp data, "application/json"
-
-#   get "/plaintext":
-#     const data = "Hello, World!"
-#     resp data, "text/plain"
-
-
-runForever()
-
-#nim c -d:release --threads:on example.nim
-#nim c -d:release --threads:on --opt:speed --stackTrace:off example.nim 
-
-
-#sudo sysctl -w fs.inotify.max_user_watches=100000
+    main()
 
 #ulimit -n 63536
-
-
-#sudo sysctl net.core.somaxconn=9086
-#sudo sysctl net.core.netdev_max_backlog=4096
-#sudo sysctl net.ipv4.tcp_max_syn_backlog=4096
-#sudo ifconfig enp3s0 txqueuelen 10000
-#sudo sysctl -p
-
-#sudo sysctl net.ipv4.tcp_fin_timeout=15
-#sudo sysctl net.ipv4.ip_local_port_range="15000 61000"
-
-#wrk -c 2046 -t 4 http://192.168.0.134:8080/json -d 400
-
-# sudo sysctl net.ipv4.tcp_tw_recycle=1
-# sudo sysctl net.ipv4.tcp_tw_reuse=1 
-#wrk -c 2046 -t 4 http://0.0.0.0:8080/json -d 400
+#nim c -d:release --threads:off --opt:speed --stackTrace:off example.nim -> for async
+#nim c -d:release --threads:on --opt:speed --stackTrace:off example.nim
+#nim c -d:release --threads:on --opt:speed --stackTrace:off --showAllMismatches:on example.nim
+#nimble install jester@#head
