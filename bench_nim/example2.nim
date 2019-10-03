765691a9fd5e9f77f218d38ae5813d6eb7bcae5c
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2019-10-03 18:18:54 -0300

    nim example 2

--- bench_nim/example2.nim
+++ bench_nim/example2.nim
@@ -1,8 +1,8 @@
-import jester  # ./jester/jester # i had to clone the freshest jester
+import jester # ./jester/jester # i had to clone the freshest jester
 import strtabs, asyncdispatch, os, strutils, json, threadpool
 import lists
 import pg
-let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
+
 var cpucores = 4
 let x: string = "test"
 # var settings = newSettings(port=Port(5000), staticDir=".") #,#reusePort = true)
@@ -19,7 +19,7 @@ proc rowizer_async(rec: seq[string]): Future[JsonNode] {.async.} =
     result = %*Row(salary: parseFloat(rec[0]), address: rec[1],
             age: parseInt(rec[2]), id: parseInt(rec[3]), name: rec[4])
 
-proc exec_query_async(): Future[seq[JsonNode]] {.async.} =
+proc exec_query_async(pool: AsyncPool): Future[seq[JsonNode]] {.async.} =
     var
         j: seq[JsonNode]
         #j {.threadvar.}: seq[JsonNode]
@@ -29,43 +29,49 @@ proc exec_query_async(): Future[seq[JsonNode]] {.async.} =
     for rec in rows:
         let row_data = await rowizer_async(rec)
         j.add(row_data)
-
+    
+    
+    pool.close()
+    
     result = j
 
 proc processLine(line: string) =
-  
-  settings:
-    port = Port(8080)
-    reusePort = true
-    #appName = "/foo"
-    bindAddr = "0.0.0.0"
-  
-  routes:
-    get "/":
-      resp("Hello World!")
-    
-    get "/db2":
-        var j_data = await exec_query_async()
-        resp $(%*j_data), "application/json"
-    
-    get "/json":
-      resp("Please use POST-method on this path.")
-    
-    get "/redirect":
-      redirect "http://192.168.0.1:80"
-    
-    post "/json":
-      var push = parseJson($request.body)
-      resp ("You have just sent this JSON: " & $push)
-    
-    post "/redirect":
-      var push = parseJson($request.body)
-      redirect "http://192.168.0.1:80"
-  
-  runForever()
+
+    settings:
+        port = Port(8080)
+        reusePort = true : bool
+        #appName = "/foo"
+        bindAddr = "0.0.0.0"
+
+
+    let pool = newAsyncPool("localhost", "gustavo", "test", "postgres", 20)
+
+    routes:
+        get "/":
+            resp("Hello World!")
+
+        get "/db2":
+            var j_data = await exec_query_async(pool)
+            resp $(%*j_data), "application/json"
+
+        get "/json":
+            resp("Please use POST-method on this path.")
+
+        get "/redirect":
+            redirect "http://192.168.0.1:80"
+
+        post "/json":
+            var push = parseJson($request.body)
+            resp ("You have just sent this JSON: " & $push)
+
+        post "/redirect":
+            var push = parseJson($request.body)
+            redirect "http://192.168.0.1:80"
+
+    runForever()
 
 var i = 1
 while i <= cpucores:
-  spawn processLine(x)
-  inc(i)
+    spawn processLine(x)
+    inc(i)
 sync()
