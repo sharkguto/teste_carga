34d3357617b8e5e8ca90a9d011f4b58bef80e2df
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2019-11-26 17:22:57 -0300

    test

--- v_bench/main.v
+++ v_bench/main.v
@@ -1,10 +1,9 @@
-
-module xxx
+module main
 
 import vweb
 
 const (
-	port = 8082
+	port = 8080
 )
 
 struct App {
@@ -14,8 +13,8 @@ pub mut:
 }
 
 fn main() {
-	mut app := App{}
-	vweb.run(app, port)
+	app := App{}
+	vweb.run(mut app, port)
 	//vweb.run<App>(Port)
 }
 
@@ -23,10 +22,9 @@ pub fn (app mut App) init() {
 	app.vweb.handle_static('.')
 }
 
-pub fn (app mut App) json_endpoint() {
+pub fn (app & App) json_endpoint() {
 	app.vweb.json('{"a": 3}')
 }
-
 
 pub fn (app mut App) index() {
 	app.cnt ++
@@ -34,9 +32,8 @@ pub fn (app mut App) index() {
 	$vweb.html()
 }
 
-
-pub fn (app mut App) text() {
-	app.vweb.text('hello world')
+pub fn (app & App) text() {
+	app.vweb.text('Hello world')
 }
 
 pub fn (app mut App) cookie() {
@@ -45,5 +42,3 @@ pub fn (app mut App) cookie() {
 	app.vweb.text(app.vweb.headers)
 	app.vweb.text('Text: hello world')
 }
-
-main()
